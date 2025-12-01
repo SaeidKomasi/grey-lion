6737caa191e59b9b088b26288f2c99b58d5c36d0
SAEID KOMASI <126557147+SaeidKomasi@users.noreply.github.com>
2025-12-01 01:47:16 +0100

    Remove Home component from page.tsx

--- app/c/page.tsx
+++ app/c/page.tsx
@@ -1,9 +1,1 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center text-white bg-black">
-      <h1 className="text-6xl font-bold">
-        🦁 Grey Lion — Power & Precision
-      </h1>
-    </main>
-  );
-}
+
